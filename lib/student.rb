31601a5781232d9f20b3d1e659ed7b931972266e
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-10 18:59:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,23 @@
 class Student
 
-  
+   attr_accessor :name, :grade, :id
+ 
+  def initialize(name, grade, id=nil)
+    @id = id
+    @name = name
+    @album = album
+  end
+ 
+  def self.create_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
+  end
+ 
+end 
   
