829286d4a1a9044e17122215b40e4dee06c447b2
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-10 18:52:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,6 +3,11 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
+  attr_accessor :name, :grade, :id
   
+  def intialize (name, grade)
+    @name = name
+    @grade = grade
+  end 
   
 end
