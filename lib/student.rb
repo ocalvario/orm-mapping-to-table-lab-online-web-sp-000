aa14b00bfb711e0896e3571e04ac7a5464bb5e5c
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-10 19:03:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,7 +9,15 @@ class Student
     @grade = grade
   end
   
-  
+    def save
+    sql = <<-SQL
+      INSERT INTO songs (name, album) 
+      VALUES (?, ?)
+    SQL
+ 
+    DB[:conn].execute(sql, self.name, self.album)
+ 
+  end
  
 end 
   
